f58483b92afb896c8d123b1fc0672eb7845f07b5
MUHAMMAD WASEET ALI <waseetali444@gmail.com>
2026-08-15 23:35:03 +0500

    Add files via upload

---
+++ bday_messages.py
@@ -0,0 +1,10 @@
+import datetime, bday_messages
+
+today = datetime.date.today()
+next_birthday = datetime.date(2026, 9, 20)
+days_away = (next_birthday - today).days
+
+if today == next_birthday:
+    print(bday_messages.random_message)
+else:
+    print(f'My next birthday is {days_away} days away!')
